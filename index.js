dca37f120b7656df47651d4089e2232ef886a568
nunu <reza.muhsinin@gmail.com>
2019-05-30 20:31:13 +0700

    add test route

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ app.get('/', function (req, res) {
   res.send('hello world')
 })
 
+app.get('/test', function (req, res) {
+    res.send('hello test')
+  })
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT)
